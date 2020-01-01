70875f6176b751354c705d4c9f3c34eda1f04532
alexniga <alex.nigactin@gmail.com>
2020-01-01 23:38:15 +0200

    server

--- server.js
+++ server.js
@@ -1,24 +1,70 @@
-// Retrieve
-var MongoClient = require('mongodb').MongoClient;
+// Import packages
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const uuid = require("uuid");
 
-var express = require('express');
-var app = express();
-var path = require('path');
+const fs = require("fs");
 
-app.use(express.static(__dirname ));
+// Aplicatia
+const app = express();
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/home.html'));
+// Middleware
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Create
+app.post("/dogs", (req, res) => {
+  const dogsList = readJSONFile();
+  // Completati cu codul vostru aici
 });
 
-app.listen("3001", () =>
-  console.log("Server started at: http://localhost:3001")
+// Read One
+app.get("/dogs/:id", (req, res) => {
+  const dogsList = readJSONFile();
+  // Completati cu codul vostru aici
+});
+
+// Read All
+app.get("/dogs", (req, res) => {
+  const dogsList = readJSONFile();
+  // Completati cu codul vostru aici
+});
+
+// Update
+app.put("/dogs/:id", (req, res) => {
+  const dogsList = readJSONFile();
+  // Completati cu codul vostru aici
+});
+
+// Delete
+app.delete("/dogs/:id", (req, res) => {
+  const dogsList = readJSONFile();
+  // Completati cu codul vostru aici
+});
+
+// Functia de citire din fisierul db.json
+function readJSONFile() {
+  return JSON.parse(fs.readFileSync("db.json"))["dogs"];
+}
+
+// Functia de scriere in fisierul db.json
+function writeJSONFile(content) {
+  fs.writeFileSync(
+    "db.json",
+    JSON.stringify({ dogs: content }),
+    "utf8",
+    err => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+}
+
+// Pornim server-ul
+app.listen("3000", () =>
+  console.log("Server started at: http://localhost:3000")
 );
-
-
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/projectDB", {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
-  if(!err) {
-    console.log("We are connected");
-  }
-});
