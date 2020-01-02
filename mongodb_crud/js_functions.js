4079aa914a9d8fbd8548cac6e2586c5b9a4bd291
miruna1224 <miruna1224@yahoo.com>
2020-01-02 20:19:48 +0200

    all

--- mongodb_crud/js_functions.js
+++ mongodb_crud/js_functions.js
@@ -18,15 +18,19 @@ function mergipls(){
    if ( req.session.user != null &&  req.session.password != null && req.session.user != null ){
      document.write ( '<a class="menu-item" href="LoginForm.html"><i class="fa fa-file-text"></i> Login </a>');
      document.write ( '<a class="menu-item" href="RegisterForm.html"><i class="fa fa-heart"></i> Signin </a>');
+     console.log( "aici");
    }
    else{
      document.write( '<a class="menu-item"><i class="fa fa-file-heart"></i> WELCOME,');
      document.write( req.session.user);
      document.write(' </a>' );
      document.write ( '<a  onclick="Logout()" class="menu-item" href="home.html"><i class="fa fa-heart"></i> LOGOUT </a>');
+     console.log ("kjhv");
    }
  }
 
 function Logout(){
   req.session.user = null;
+    req.session.email = null;
+      req.session.user = null;
 }
